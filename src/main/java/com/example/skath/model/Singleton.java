98ce05c41efb4d476f614cb9840517dff5588637
DiegoZM1006 <1109184718@u.icesi.edu.co>
2022-12-26 20:10:55 -0500

    Adding the Singleton class

---
+++ src/main/java/com/example/skath/model/Singleton.java
@@ -0,0 +1,64 @@
+package com.example.skath.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Singleton {
+
+    private static Singleton instance;
+    private Connection cn;
+    private User user;
+    private boolean isSideBarCollapsed;
+
+    private Singleton() {
+        isSideBarCollapsed = false;
+    }
+
+    public static Singleton getInstance() {
+        if(instance == null) {
+            instance = new Singleton();
+            instance.connectionToDB();
+        }
+        return instance;
+    }
+
+    private void connectionToDB() {
+        try {
+            String urlDb = "jdbc:mysql://localhost/db_mysystem";
+            String username = "root";
+            String password = "";
+            cn = DriverManager.getConnection(urlDb, username, password);
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public Connection getCn() {
+        return cn;
+    }
+
+    public void setCn(Connection cn) {
+        this.cn = cn;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public static void setInstance(Singleton instance) {
+        Singleton.instance = instance;
+    }
+
+    public boolean isSideBarCollapsed() {
+        return isSideBarCollapsed;
+    }
+
+    public void setSideBarCollapsed(boolean sideBarCollapsed) {
+        isSideBarCollapsed = sideBarCollapsed;
+    }
+
+}
