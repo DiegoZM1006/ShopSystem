bfc0da808e9abce0f413a565b1f4b1deb09f26d2
DiegoZM1006 <1109184718@u.icesi.edu.co>
2023-01-04 11:56:06 -0500

    Adding the fxml and controller of the Storage section

---
+++ src/main/java/com/example/skath/controller/StorageController.java
@@ -0,0 +1,21 @@
+package com.example.skath.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class StorageController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    void callData() {
+
+        // Llamando los almacenes de la base de datos
+
+    }
+
+}
