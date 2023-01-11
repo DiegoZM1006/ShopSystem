227d533059dca21325e3f0bec764fe101b13af0a
DiegoZM1006 <1109184718@u.icesi.edu.co>
2023-01-10 21:18:42 -0500

    Section clients COMPLETED

--- src/main/java/com/example/skath/controller/ClientsController.java
+++ src/main/java/com/example/skath/controller/ClientsController.java
@@ -1,14 +1,26 @@
 package com.example.skath.controller;
 
 import com.example.skath.MainApplication;
-import com.example.skath.model.Singleton;
+import com.example.skath.model.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class ClientsController implements Initializable {
@@ -16,9 +28,99 @@ public class ClientsController implements Initializable {
     @FXML
     private VBox sideBar;
 
+    private Connection cn;
+
+    // Buttons form
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private Button btnUpdate;
+
+    // Fields form
+
+    @FXML
+    private TextField idTF;
+
+    @FXML
+    private TextField nameTF;
+
+    // TableView
+
+    @FXML
+    private TableView<Client> table;
+
+    @FXML
+    private TableColumn<Client, String> name;
+
+    @FXML
+    private TableColumn<Client, Integer> ID;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         btnMenu(new ActionEvent());
+
+        idTF.setDisable(true);
+
+        btnUpdate.setDisable(true);
+        btnDelete.setDisable(true);
+
+        try {
+            callData();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    void callData() throws SQLException {
+
+        // Establecemos la conexion
+        cn = Singleton.getInstance().getCn();
+
+        ArrayList<Client> clients = new ArrayList<>();
+
+        String query = "SELECT * FROM client";
+        PreparedStatement pstmt = cn.prepareStatement(query);
+        ResultSet result = pstmt.executeQuery();
+
+        while(result.next()) {
+            clients.add(new Client(
+               result.getInt("ID"), result.getString("NAME")
+            ));
+        }
+
+        ObservableList<Client> obsClients = FXCollections.observableArrayList();
+        obsClients.addAll(clients);
+
+        ID.setCellValueFactory(new PropertyValueFactory<>("ID"));
+        name.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        table.setItems(obsClients);
+
+        manageEvents();
+
+        // Cerramos la conexion
+        Singleton.getInstance().closeCn();
+
+    }
+    void manageEvents(){
+        table.getSelectionModel().selectedItemProperty().addListener(
+                (observableValue, prevValue, selectedValue) -> {
+                    if (selectedValue != null){
+                        idTF.setText(String.valueOf(selectedValue.getID()));
+                        nameTF.setText(selectedValue.getName());
+
+                        btnSave.setDisable(true);
+                        btnUpdate.setDisable(false);
+                        btnDelete.setDisable(false);
+                    }
+                }
+        );
     }
 
     void btnShowWindow(String fxml, String title) {
@@ -32,6 +134,112 @@ public class ClientsController implements Initializable {
         MainApplication.showWindow(fxml, title, true,true);
         Stage currentStage = (Stage) sideBar.getScene().getWindow();
         currentStage.hide();
+    }
+
+    // Form buttons actions
+
+    @FXML
+    void btnSave(ActionEvent event) throws SQLException {
+
+        if(!nameTF.getText().equals("")) {
+
+            // Establecemos la conexion
+            cn = Singleton.getInstance().getCn();
+
+            String query = "INSERT INTO client (NAME) values (?)";
+            PreparedStatement pstmt = cn.prepareStatement(query);
+            pstmt.setString(1, nameTF.getText());
+            int result = pstmt.executeUpdate();
+
+            if(result == 1) {
+                Alerts.info("Registro agregado", "El registro fue EXITOSAMENTE agregado");
+
+                btnShowWindow("clients.fxml", "Clientes");
+
+            } else {
+                Alerts.error("Error", "Ha ocurrido un error inesperado en el sistema, el registro NO se guardo");
+            }
+
+            // Cerramos la conexion
+            Singleton.getInstance().closeCn();
+
+        } else {
+            Alerts.warning("Error", "Rellena todos los campos");
+        }
+
+    }
+
+    @FXML
+    void btnUpdate(ActionEvent event) throws SQLException {
+
+        if(!nameTF.getText().equals("")) {
+
+            // Establecemos la conexion
+            cn = Singleton.getInstance().getCn();
+
+            String query = "UPDATE client SET NAME = ? WHERE ID = ?";
+            PreparedStatement pstmt = cn.prepareStatement(query);
+            pstmt.setString(1, nameTF.getText());
+            pstmt.setInt(2, Integer.parseInt(idTF.getText()));
+            int result = pstmt.executeUpdate();
+
+            if(result == 1) {
+
+                Alerts.info("Registro actualizado", "El registro fue EXITOSAMENTE actualizado");
+
+                btnShowWindow("clients.fxml", "Clientes");
+
+            } else {
+                Alerts.error("Error", "Ha ocurrido un error inesperado en el sistema, el registro NO se actualizo");
+            }
+
+            // Cerramos la conexion
+            Singleton.getInstance().closeCn();
+
+        } else {
+            Alerts.warning("Error", "Rellena todos los campos");
+        }
+
+    }
+
+    @FXML
+    void btnDelete(ActionEvent event) throws SQLException {
+
+        // Establecemos la conexion
+        cn = Singleton.getInstance().getCn();
+
+        String query = "DELETE FROM client WHERE ID = ?";
+        PreparedStatement pstmt = cn.prepareStatement(query);
+        pstmt.setInt(1, Integer.parseInt(idTF.getText()));
+        int result = pstmt.executeUpdate();
+
+        cn.close();
+
+        if(result == 1) {
+
+            Alerts.info("Registro eliminado", "El registro fue EXITOSAMENTE eliminado");
+
+            btnShowWindow("clients.fxml", "Clientes");
+
+        } else {
+            Alerts.error("Error", "Ha ocurrido un error inesperado en el sistema, el registro NO se elimino");
+        }
+
+        // Cerramos la conexion
+        Singleton.getInstance().closeCn();
+
+    }
+
+    @FXML
+    void btnNew(ActionEvent event) {
+
+        idTF.setText(null);
+        nameTF.setText(null);
+
+        btnSave.setDisable(false);
+        btnUpdate.setDisable(true);
+        btnDelete.setDisable(true);
+
     }
 
     // Buttons actions
